35a3c8d1cb1d956225acb04a8d8435eba197c34b
wirelessdreamer <wirelessdreamer@e47b612d-987e-4a75-91bb-c5916b235968>
2011-01-10 23:19:26 +0000

    inital g2ghpro commit, still has a bug where chording in game isn't reliable, preparing to test windows builds

---
+++ src/g2ghpro.cpp
@@ -0,0 +1,192 @@
+/*****************************************
+g2ghpro.cpp
+
+Midi Guitar to Rock Band Pro Guitar converter
+
+by Nathanael Anderson
+
+wirelessdreamer @t gmai<L> d.t com
+
+*****************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include "RtMidi.h"
+
+RtMidiOut *midiout;
+bool chooseMidiPort( RtMidiOut *rtmidi );
+void usage( void ) {
+	// Error function in case of incorrect command-line
+	// argument specifications.
+	std::cout << "\nuseage: g2ghpro <port>\n";
+	std::cout << "    where port = the device to use (default = 0).\n\n";
+	exit( 0 );
+}
+
+void mycallback( double deltatime, std::vector< unsigned char > *message, void *userData)
+{
+	int base[6];
+	base[5] = 40;
+	base[4] = 45;
+	base[3] = 50;
+	base[2] = 55;
+	base[1] = 59;
+	base[0] = 64;
+
+	std::vector<unsigned char> oMessage;
+	std::vector<unsigned char> o1Message;
+	std::vector<unsigned char> nMessage;
+	int note = (int)message->at(1);
+	int velocity = (int)message->at(2);
+	velocity = 110;
+	int type = (int)message->at(0);
+	int channel = -1;
+
+	if ((type >= 144) && (type <= 159)){ // Note On Event
+		std::cout << "Note: " << note << std::endl;
+		channel = type - 144;
+		std::vector<unsigned char> sysExMessage;
+		sysExMessage.push_back( 240);
+		sysExMessage.push_back( 8 );
+		sysExMessage.push_back( 64 ); 
+		sysExMessage.push_back( 10  );
+		sysExMessage.push_back( 1); // 1 sets fret position
+		sysExMessage.push_back( channel + 1 ); // channel
+		sysExMessage.push_back( note );
+		sysExMessage.push_back( 247 );
+		midiout->sendMessage( &sysExMessage );
+
+		std::vector<unsigned char> sysExMessage1;
+		sysExMessage1.push_back( 240);
+		sysExMessage1.push_back( 8 );
+		sysExMessage1.push_back( 64 );
+		sysExMessage1.push_back( 10  );
+		sysExMessage1.push_back( 5); // 5 for playing a string
+		sysExMessage1.push_back( channel + 1 ); // channel
+		sysExMessage1.push_back( velocity );
+		sysExMessage1.push_back( 247 );
+		midiout->sendMessage( &sysExMessage1 );
+	}else if ((type >= 128) && (type <= 143)){ // Note Off Event
+		channel = type - 128;
+		if (note != base[channel]){ // no need to return the fret pushed down back to open, if already open
+			std::vector<unsigned char> sysExMessage;
+			sysExMessage.push_back( 240);
+			sysExMessage.push_back( 8 );
+			sysExMessage.push_back( 64 );
+			sysExMessage.push_back( 10  );
+			sysExMessage.push_back(1);
+			sysExMessage.push_back( channel + 1);
+			sysExMessage.push_back( velocity );
+			sysExMessage.push_back( 247 );
+			midiout->sendMessage( &sysExMessage );
+		}
+	}
+}
+
+int main( int argc, char *argv[] )
+{
+	RtMidiIn *midiin = 0;
+
+	// Minimal command-line check.
+	if ( argc > 2 ) usage();
+
+	// RtMidiIn constructor
+	try {
+		midiin = new RtMidiIn();
+	}
+	catch ( RtError &error ) {
+		error.printMessage();
+		exit( EXIT_FAILURE );
+	}
+
+	// Check available ports vs. specified.
+	unsigned int port = 0;
+	unsigned int nPorts = midiin->getPortCount();
+	if ( argc == 2 ) port = (unsigned int) atoi( argv[1] );
+	if ( port >= nPorts ) {
+		delete midiin;
+		std::cout << "Invalid port specifier!\n";
+		usage();
+	}
+
+	try {
+		midiin->openPort( port );
+	}
+	catch ( RtError &error ) {
+		error.printMessage();
+		goto cleanup;
+	}
+
+	// RtMidiOut constructor
+	try {
+		midiout = new RtMidiOut();
+	}
+	catch ( RtError &error ) {
+		error.printMessage();
+		exit( EXIT_FAILURE );
+	}
+	// Call function to select port.
+	try {
+		if ( chooseMidiPort( midiout ) == false ) goto cleanup;
+	}
+	catch ( RtError &error ) {
+		error.printMessage();
+		goto cleanup;
+	}
+	// Set our callback function.  This should be done immediately after
+	// opening the port to avoid having incoming messages written to the
+	// queue instead of sent to the callback function.
+	midiin->setCallback( &mycallback );
+
+	// Don't ignore sysex, timing, or active sensing messages.
+
+	midiin->ignoreTypes( false, false, false );
+
+	std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
+	char input;
+	std::cin.get(input);
+
+	std::cout << "Ready to send test" << std::endl;
+	std::cin.get(input);
+
+	std::cin.get(input);
+
+	// Clean up
+cleanup:
+	delete midiin;
+	delete midiout;
+
+	return 0;
+}
+
+bool chooseMidiPort( RtMidiOut *rtmidi )
+{
+	std::string portName;
+	unsigned int i = 0, nPorts = rtmidi->getPortCount();
+	if ( nPorts == 0 ) {
+		std::cout << "No output ports available!" << std::endl;
+		return false;
+	}
+
+	if ( nPorts == 1 ) {
+		std::cout << "\nOpening " << rtmidi->getPortName() << std::endl;
+	}
+	else {
+		for ( i=0; i<nPorts; i++ ) {
+			portName = rtmidi->getPortName(i);
+			std::cout << "  Output port #" << i << ": " << portName << '\n';
+		}
+
+		do {
+			std::cout << "\nChoose a port number: ";
+			std::cin >> i;
+		} while ( i >= nPorts );
+	}
+
+	std::cout << "\n";
+	rtmidi->openPort( i );
+
+	return true;
+}
+
+
