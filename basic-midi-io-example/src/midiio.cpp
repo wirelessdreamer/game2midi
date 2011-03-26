a7fd401dd3ba774c447ef9a504c5297ed4871de6
wirelessdreamer <wirelessdreamer@e47b612d-987e-4a75-91bb-c5916b235968>
2011-03-26 14:24:25 +0000

    added midioutput example

--- basic-midi-io-example/src/midiio.cpp
+++ basic-midi-io-example/src/midiio.cpp
@@ -22,48 +22,14 @@ int mode = 0;
 void usage( void ) {
 	// Error function in case of incorrect command-line
 	// argument specifications.
-	std::cout << "\nuseage: g2ghpro <-m MODE>\n";
-	std::cout << "    Mode Values:\n";
-	std::cout << "    0 = electric guitar\n";
-	std::cout << "    1 = 4 string bass\n";
-	std::cout << "    2 = 5 string bass\n";
-	std::cout << "    3 = 6 string bass\n";
+	std::cout << "\nPut a useage message here that is useful\n";
 	exit( 0 );
 }
 
 void mycallback( double deltatime, std::vector< unsigned char > *message, void *userData)
 {
-	int base[6]; // these are the notes the 6 strings have when played open, based on channel
-	if (mode == 0){ // guitar mode
-		base[5] = 40; // so channel 5's (low e) midi note number is 40
-		base[4] = 45; // channel 4 - A midi note is 45
-		base[3] = 50; // channel 3 - D midi note is 45
-		base[2] = 55; // G
-		base[1] = 59; // B
-		base[0] = 64; // E (high)
-	}else if(mode == 1){ // 4 string bass mode
-		base[3] = 40; // so channel 5's (low e) midi note number is 40
-		base[2] = 45; // channel 4 - A midi note is 45
-		base[1] = 50; // channel 3 - D midi note is 45
-		base[0] = 55; // G
-	}else if(mode == 2){ // 5 string bass mode
-		base[4] = 40; // so channel 5's (low e) midi note number is 40
-		base[3] = 45; // channel 4 - A midi note is 45
-		base[2] = 50; // channel 3 - D midi note is 45
-		base[1] = 55; // G
-	}else if(mode == 3){ // 6 string bass mode
-		base[5] = 40; // so channel 5's (low e) midi note number is 40
-		base[4] = 45; // channel 4 - A midi note is 45
-		base[3] = 50; // channel 3 - D midi note is 45
-		base[2] = 55; // G
-	}
-
-	std::vector<unsigned char> oMessage;
-	std::vector<unsigned char> o1Message;
-	std::vector<unsigned char> nMessage;
 	int note = (int)message->at(1);
 	int velocity = (int)message->at(2);
-	velocity = 110;
 	int type = (int)message->at(0);
 	int channel = -1;
 
@@ -81,61 +47,8 @@ void mycallback( double deltatime, std::vector< unsigned char > *message, void *
 	if ((type >= 144) && (type <= 159)){ // Note On Event
 		std::cout << "Note: " << note << std::endl;
 		channel = type - 144; // based on the note type we figure out the channel, as mentioned above
-
-		/* Rock Band 3 Pro guitar SysEx Message Format
-		 *
-		 * Part    1  2  3  4 5 6  7  8
-		 * Sample 240 8 64 10 1 1 43 247
-		 *
-		 * Part 1 - Starting byte of a SysEx Message (always 240)
-		 * Part 2,3,4 - Identifiers that this is a SysEx message used for Pro Guitar 
-		 * Part 5 - Message type (1 = set fret position, 5 = play string)
-		 * Part 6 - Midi Channel
-		 * Part 7 - Midi Note
-		 * Part 8 - End SysEx Message (always 247)
-		*/
-
-		std::vector<unsigned char> sysExMessage;
-		sysExMessage.push_back( 240);
-		sysExMessage.push_back( 8 );
-		sysExMessage.push_back( 64 ); 
-		sysExMessage.push_back( 10  );
-		sysExMessage.push_back( 1); // 1 sets fret position
-		sysExMessage.push_back( channel + 1 ); // channel
-		sysExMessage.push_back( note );
-		sysExMessage.push_back( 247 );
-		midiout->sendMessage( &sysExMessage );
-
-		std::vector<unsigned char> sysExMessage1;
-		sysExMessage1.push_back( 240);
-		sysExMessage1.push_back( 8 );
-		sysExMessage1.push_back( 64 );
-		sysExMessage1.push_back( 10  );
-		sysExMessage1.push_back( 5); // 5 for playing a string
-		sysExMessage1.push_back( channel + 1 ); // channel
-		sysExMessage1.push_back( velocity );
-		sysExMessage1.push_back( 247 );
-		midiout->sendMessage( &sysExMessage1 );
 	}else if ((type >= 128) && (type <= 143)){ // Note Off Event
 		channel = type - 128;
-		/* 	Here if a note other then the open string was played 
-			we return the state of that string to open 
-			This is for the display in game of what frets are 
-			pushed down, but easily gets out of sync from 
-			ghost notes
-		 */
-		if (note != base[channel]){ // no need to return the fret pushed down back to open, if already open
-			std::vector<unsigned char> sysExMessage;
-			sysExMessage.push_back( 240);
-			sysExMessage.push_back( 8 );
-			sysExMessage.push_back( 64 );
-			sysExMessage.push_back( 10  );
-			sysExMessage.push_back(1);
-			sysExMessage.push_back( channel + 1);
-			sysExMessage.push_back( velocity );
-			sysExMessage.push_back( 247 );
-			midiout->sendMessage( &sysExMessage );
-		}
 	}
 }
 
@@ -143,7 +56,7 @@ int main( int argc, char *argv[] )
 {
 	extern char *optarg;
 	int c;
-
+	// example passing option -m NUMBER
 	while ((c = getopt(argc, argv, "m:")) != -1){
 		switch (c){
 			case 'm': 
